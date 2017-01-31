a037cb8ea5f6521824fe6df1513faacd4e183108
hamidb <hamidb@uci.edu>
2017-01-31 11:04:39 -0600

    In-class-assignment-Android

---
+++ app/src/main/java/com/example/hamid/inclassassignment/ValidActivity.java
@@ -0,0 +1,36 @@
+package com.example.hamid.inclassassignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ValidActivity extends AppCompatActivity {
+
+    private TextView validTV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valid);
+
+        validTV = (TextView) findViewById(R.id.validTV);
+        //get the extra data from the received Intent
+
+        Intent receivedIntent = getIntent();
+        if(receivedIntent != null){
+            //get the username
+            String username = receivedIntent.getStringExtra("U");
+            validTV.setText("You have successfully logged in.");
+            //send broadcast Intent
+            Intent broadcastIntent = new Intent("Welcome");
+            broadcastIntent.putExtra("U", username);
+            sendBroadcast(broadcastIntent);
+        }
+
+
+
+
+
+    }
+}
+
